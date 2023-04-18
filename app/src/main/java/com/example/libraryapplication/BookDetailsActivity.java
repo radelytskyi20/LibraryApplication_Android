857e6f4d73e42d3df0f0322889c7391b1c10fd19
Yaroslav <vip.radelickiy@gmail.com>
2023-04-18 22:42:23 +0300

    added title for toolbar in Book Details Activity

--- app/src/main/java/com/example/libraryapplication/BookDetailsActivity.java
+++ app/src/main/java/com/example/libraryapplication/BookDetailsActivity.java
@@ -10,5 +10,6 @@ public class BookDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_details);
+        getSupportActionBar().setTitle("Загальна інформація");
     }
 }
